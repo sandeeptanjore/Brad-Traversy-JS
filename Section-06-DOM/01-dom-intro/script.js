0d29b8ecc1200e3201872ce9fb844c883826328a
Sandeep Tanjore <sandeep.tanjore@mcgill.ca>
2024-11-04 14:02:15 -0500

    Started the first lesson in DOM

---
+++ Section-06-DOM/01-dom-intro/script.js
@@ -0,0 +1,12 @@
+// console.dir(window.document.body);
+// console.dir(window.document.body.innerHTML);
+// console.log(document.body.innerText);
+
+console.log(document.links[0]);
+
+// document.body.innerHTML = '<h1>Hello World</h1>';
+
+console.log(document.getElementById('main'));
+
+const main = document.getElementById('main');
+main.innerHTML = '<h1>Hello from Sandeep</h1>';

dd8b61930414c4b0c13fc6c56d6d626497d67210
Sandeep Tanjore <sandeep.tanjore@mcgill.ca>
2024-11-04 15:34:56 -0500

    Started the first lesson in DOM

---
+++ Section-06-DOM/03-dom-selectors-single-element/script.js
@@ -0,0 +1,11 @@
+let output;
+
+output=document.all[10];
+output = document.head
+output= document.body;
+output=document.head.children;
+output= document.body.children;
+
+output= document.forms;
+
+console.log(output)

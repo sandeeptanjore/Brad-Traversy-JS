21e994853a2e1286f5124dbec9597c4c95430861
Sandeep Tanjore <sandeep.tanjore@mcgill.ca>
2024-11-05 15:05:34 -0500

    Continuing DOM lessons

--- Section-06-DOM/03-dom-selectors-single-element/script.js
+++ Section-06-DOM/03-dom-selectors-single-element/script.js
@@ -1,11 +1,45 @@
-let output;
+//document.getElementById()
 
-output=document.all[10];
-output = document.head
-output= document.body;
-output=document.head.children;
-output= document.body.children;
+console.log(document.getElementById('app-title'))
+console.log(document.getElementById('app-title').getAttribute('id'))
 
-output= document.forms;
+//Set attributes
+document.getElementById('app-title').title= 'Sandeeps Shopping List';
 
-console.log(output)
+document.getElementById('app-title').setAttribute('class', 'title');
+
+const title= document.getElementById('app-title');
+console.log(title);
+
+//Get/Set content
+console.log(title.textContent)
+title.textContent = 'Hi Sandeep'
+title.innerText = 'Hi again'
+title.innerHTML = '<strong>Shopping List</strong>'
+
+//Change style
+title.style.color = 'tomato'
+title.style.backgroundColor ='black'
+title.style.padding = '10px'
+title.style.borderRadius = '10px'
+
+//document.querySelector()
+
+console.log(document.querySelector('h1')) 
+console.log(document.querySelector('#app-title')) 
+console.log(document.querySelector('.container')) 
+console.log(document.querySelector('input[type="text"]')) 
+console.log(document.querySelector('li:nth-child(2)')) 
+console.log(document.querySelector('li:nth-child(2)').innerText) 
+console.log(document.querySelector('li:nth-child(3)'));
+
+const secondItem= document.querySelector('li:nth-child(2)')
+secondItem.innerText ='Apple Santra Mosambi'
+secondItem.style.color = 'red'
+
+//Use these methods on other elements
+const list = document.querySelector('ul');
+console.log(list)
+const firstItem=list.querySelector('li');
+// console.log(firstItem) 
+firstItem.style.color ='blue'

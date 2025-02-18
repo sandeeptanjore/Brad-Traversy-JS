51cab4fc5cf92ec67f455b8680c51b34b9594ef5
Sandeep Tanjore <xyz@gmail.com>
2025-02-17 21:08:28 -0500

    Brad Traversy's JS lessons

--- Section-08-Shopping-List-Project/script.js
+++ Section-08-Shopping-List-Project/script.js
@@ -1,6 +1,38 @@
 const itemForm = document.getElementById('item-form');
-const itemInput = document.getElementById('item-inoput');
+const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 
+const addItem = (e) => {
+  e.preventDefault();
+
+  //validate input
+  if (itemInput.value === '') {
+    itemInput.placeholder = 'Please add an item';
+    //alert('Please add an item');
+    return;
+  }
+
+  //Create list item
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(itemInput.value));
+  const button = createButton('remove-item btn-link text-red');
+  li.appendChild(button);
+  itemList.appendChild(li);
+  itemInput.value = '';
+};
+
+const createButton = (classes) => {
+  const button = document.createElement('button');
+  button.className = classes;
+  const icon = createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);
+  return button;
+};
+
+const createIcon = (classes) => {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
+};
 //Event Listeners
 itemForm.addEventListener('submit', addItem);

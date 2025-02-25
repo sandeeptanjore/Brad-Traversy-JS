b4a63beeb58a3907818cec9a2c7ac5e6e5c4c6db
Sandeep Tanjore <xyz@gmail.com>
2025-02-25 05:13:42 -0500

    Brad Travery's JS lessons

--- Section-08-Shopping-List-Project/script.js
+++ Section-08-Shopping-List-Project/script.js
@@ -1,7 +1,10 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
+//Adding a new item
 const addItem = (e) => {
   e.preventDefault();
 
@@ -17,8 +20,33 @@ const addItem = (e) => {
   li.appendChild(document.createTextNode(itemInput.value));
   const button = createButton('remove-item btn-link text-red');
   li.appendChild(button);
+
+  //Add li to the DOM
   itemList.appendChild(li);
+
+  checkUI();
+
   itemInput.value = '';
+};
+
+//Removing an items
+
+const removeItem = (e) => {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    if (confirm('Are you sure...?')) {
+      e.target.parentElement.parentElement.remove();
+      checkUI();
+    }
+  }
+};
+
+//Clearing items
+
+const clearItems = () => {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+  checkUI();
 };
 
 const createButton = (classes) => {
@@ -34,5 +62,36 @@ const createIcon = (classes) => {
   icon.className = classes;
   return icon;
 };
+
+const filterItems = (e) => {
+  const items = itemList.querySelectorAll('li');
+  const text = e.target.value.toLowerCase();
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+};
+
+const checkUI = () => {
+  const items = itemList.querySelectorAll('li');
+  if (items.length === 0) {
+    clearBtn.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
+    clearBtn.style.display = 'block';
+    itemFilter.style.display = 'block';
+  }
+};
+
 //Event Listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
+
+checkUI();
